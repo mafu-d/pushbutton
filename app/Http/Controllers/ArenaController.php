48eb28232614b003eb1fb5c4733648ea0278cd98
Matt Dawkins <matthew.dawkins@gmail.com>
2017-06-11 18:23:18 +0100

    Add ability to create a new arena

--- app/Http/Controllers/ArenaController.php
+++ app/Http/Controllers/ArenaController.php
@@ -10,7 +10,15 @@ class ArenaController extends Controller
         return view('arenas.create');
     }
 
-    public function create() {
-        
+    public function create(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|unique:users,name'
+        ]);
+        $arena = new \App\Arena();
+        $arena->name = $request->get('name');
+        $arena->creator_id = \Auth::user()->id;
+        $arena->private = $request->get('private') === 1 ? true : false;
+        $arena->save();
+        return redirect(action('HomeController@index'));
     }
 }
